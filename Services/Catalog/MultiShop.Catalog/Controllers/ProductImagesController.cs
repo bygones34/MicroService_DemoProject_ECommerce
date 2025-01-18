1bfbbd87806f79c0a948558b134bdab8cbf30448
Alper Yusuf Dursun <AlperYusuf.Dursun@aydinli.com.tr>
2025-01-18 11:47:39 +0300

    committing up-to-date

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.DTOs.ProductImageDTOs;
+using MultiShop.Catalog.Services.ProductImageServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+        private readonly IProductImageService _productImageService;
+        public ProductImagesController(IProductImageService productImageService)
+        {
+            _productImageService = productImageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProductImagesAsync()
+        {
+            var values = await _productImageService.GetAllProductImagesAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageByIdAsync(string id)
+        {
+            var values = _productImageService.GetByIdProductImageAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImageAsync(CreateProductImageDto createProductImageDto)
+        {
+            await _productImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Product image was created successfully!");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImageAsync(string id)
+        {
+            await _productImageService.DeleteProductImageAsync(id);
+            return Ok("Product image was deleted successfully!");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImageAsync(UpdateProductImageDto updateProductImageDto)
+        {
+            await _productImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("Product image was updated successfully!");
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductImageServices/IProductImageService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Catalog.DTOs.ProductImageDTOs;
+
+namespace MultiShop.Catalog.Services.ProductImageServices
+{
+    public interface IProductImageService
+    {
+        Task<List<ResultProductImageDto>> GetAllProductImagesAsync();
+        Task<GetByIdProductImageDto> GetByIdProductImageAsync(string id);
+        Task CreateProductImageAsync(CreateProductImageDto createProductImageDto);
+        Task UpdateProductImageAsync(UpdateProductImageDto updateProductImageDto);
+        Task DeleteProductImageAsync(string id);
+    }
+}
