fe1c43dc89425990acc0362f9707bef97f24db98
Alper DURSUN <alperdursuun@gmail.com>
2025-01-18 14:34:38 +0300

    discount microservice

---
+++ Services/Discount/MultiShop.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Discount.DTOs;
+using MultiShop.Discount.Services;
+
+namespace MultiShop.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+
+        public DiscountsController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDiscountCouponsAsync()
+        {
+            var values = await _discountService.GetAllDiscountCouponsAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdDiscountCouponAsync(int id)
+        {
+            var value = await _discountService.GetByIdDiscountCouponAsync(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto)
+        {
+            await _discountService.CreateDiscountCouponAsync(createCouponDto);
+            return Ok("Discount coupon was created successfully!");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDiscountCouponAsync(int id)
+        {
+            await _discountService.DeleteDiscountCouponAsync(id);
+            return Ok("Discount coupon was deleted successfully!");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto)
+        {
+            await _discountService.UpdateDiscountCouponAsync(updateCouponDto);
+            return Ok("Discount coupon was updated successfully!");
+        }
+    }
+}
